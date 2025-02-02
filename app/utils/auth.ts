6d121611728d51535537db984f9b3b8a2652a2f6
unknown <amazighmalek.com>
2025-02-02 12:59:14 +0100

    prisma database

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
-// import { prisma } from "./db";
+ import { PrismaAdapter } from "@auth/prisma-adapter";
 import Google from "next-auth/providers/google";
+import { prisma } from "./db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-//   adapter: PrismaAdapter(prisma),
+   adapter: PrismaAdapter(prisma),
   providers: [GitHub, Google],
 });
